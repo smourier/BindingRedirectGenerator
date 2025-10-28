5863e2f1c18317e6ce6a3dd1a882d56859c4e448
Simon Mourier <simon_mourier@hotmail.com>
2025-10-28 15:31:44 +0100

    Updated to .NET 9.0

--- BindingRedirectGenerator/Program.cs
+++ BindingRedirectGenerator/Program.cs
@@ -4,152 +4,156 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Xml.Linq;
-using BindingRedirectGenerator.Utilities;
 using Mono.Cecil;
-
 // we use cecil because System.Reflection.MetaData crashes...
 
-namespace BindingRedirectGenerator
+namespace BindingRedirectGenerator;
+
+public static class Program
 {
-    public static class Program
+    /// <summary> Xml Namespace </summary>
+    public const string ns = "urn:schemas-microsoft-com:asm.v1";
+
+    public const string AttribCulture = "culture";
+    public const string AttribName = "name";
+    public const string AttribPublicKeyToken = "publicKeyToken";
+
+    public static readonly XName NameDependentAssembly = XName.Get("dependentAssembly", ns);
+    public static readonly XName NameAssemblyIdentity = XName.Get("assemblyIdentity", ns);
+    public static readonly XName NameBindingRedirect = XName.Get("bindingRedirect", ns);
+
+    /// <inheritdoc cref="ReWriteBindingRedirects"/>
+    public static void Main(string[] args)
     {
-        /// <summary> Xml Namespace </summary>
-        public const string ns = "urn:schemas-microsoft-com:asm.v1";
-
-        public const string AttribCulture = "culture";
-        public const string AttribName = "name";
-        public const string AttribPublicKeyToken = "publicKeyToken";
-
-        public static readonly XName NameDependentAssembly = XName.Get("dependentAssembly", ns);
-        public static readonly XName NameAssemblyIdentity = XName.Get("assemblyIdentity", ns);
-        public static readonly XName NameBindingRedirect = XName.Get("bindingRedirect", ns);
-
-        /// <inheritdoc cref="ReWriteBindingRedirects"/>
-        public static void Main(string[] args)
+        try
         {
-            try
+            if (args.Length < 1)
             {
-                if (args.Length < 1) {
-                    AssemblyName entryAssembly = Assembly.GetEntryAssembly()!.GetName();
-                    Console.WriteLine(entryAssembly.Name + " <config file path> [<search directory path>]");
-                    return;
-                }
-
-                var outputFilePath = new FileInfo(Path.GetFullPath(args[0]));
-                var inputDirectoryPath = args.Length > 1 ? new DirectoryInfo(args[1]) : null;
-
-                Console.WriteLine("Input       : " + inputDirectoryPath);
-                Console.WriteLine("Output      : " + outputFilePath);
-                Console.WriteLine();
-
-                ReWriteBindingRedirects(outputFilePath);
+                var entryAssembly = Assembly.GetEntryAssembly()!.GetName();
+                Console.WriteLine(entryAssembly.Name + " <config file path> [<search directory path>]");
+                return;
             }
-            catch (Exception e)
+
+            var outputFilePath = new FileInfo(Path.GetFullPath(args[0]));
+            var inputDirectoryPath = args.Length > 1 ? new DirectoryInfo(args[1]) : null;
+
+            Console.WriteLine("Input       : " + inputDirectoryPath);
+            Console.WriteLine("Output      : " + outputFilePath);
+            Console.WriteLine();
+
+            ReWriteBindingRedirects(outputFilePath);
+        }
+        catch (Exception e)
+        {
+            Console.Error.WriteLine(e);
+        }
+    }
+
+    /// <summary> Rewrites binding Redirects in the <paramref name="configFile"/> for all Assemblies (DLLs and EXEs) in the <paramref name="searchDirectory"/> </summary>
+    public static void ReWriteBindingRedirects(this FileInfo configFile, DirectoryInfo? searchDirectory = null)
+    {
+        ArgumentNullException.ThrowIfNull(configFile);
+
+        var doc = configFile.Exists ? XDocument.Load(configFile.FullName) : new XDocument();
+
+        searchDirectory ??= configFile.Directory ?? throw new ArgumentException(null, nameof(configFile));
+        var configuration = doc.GetOrCreateElement("configuration");
+        var runtime = configuration.GetOrCreateElement("runtime");
+        var assemblyBinding = runtime.GetOrCreateElement(XName.Get("assemblyBinding", ns));
+
+        foreach (var file in searchDirectory.EnumerateFiles("*.*", SearchOption.AllDirectories))
+        {
+            var ext = file.Extension;
+            if (string.Compare(".dll", ext, StringComparison.OrdinalIgnoreCase) != 0 &&
+                string.Compare(".exe", ext, StringComparison.OrdinalIgnoreCase) != 0)
             {
-                Console.Error.WriteLine(e);
+                Trace.TraceInformation("Ignoring: " + file);
+                continue;
             }
+
+            var asm = LoadAssembly(file);
+            if (asm == null) // not .NET, not valid, etc.
+                continue;
+
+            if (asm.Name.PublicKeyToken == null || asm.Name.PublicKeyToken.Length == 0) // no strong name
+            {
+                Console.WriteLine("Skipping '" + file + "': No public key token.");
+                continue;
+            }
+
+            var pkt = string.Join(string.Empty, asm.Name.PublicKeyToken.Select(i => i.ToString("x2")));
+            Func<XElement, bool> cultureFunc;
+            if (string.IsNullOrEmpty(asm.Name.Culture))
+            {
+                cultureFunc = i => i.Attribute(AttribCulture) == null || i.Attribute(AttribCulture)?.Value == "neutral";
+            }
+            else
+            {
+                cultureFunc = i => i.Attribute(AttribCulture)?.Value == asm.Name.Culture;
+            }
+
+            var dependentAssembly = assemblyBinding
+                .Descendants(NameDependentAssembly)
+                .Descendants(NameAssemblyIdentity)
+                .FirstOrDefault(i => i.Attribute(AttribName)?.Value == asm.Name.Name && i.Attribute(AttribPublicKeyToken)?.Value == pkt && cultureFunc(i))?
+                .Parent;
+            if (dependentAssembly != null)
+            {
+                Console.WriteLine("Ignoring '" + file + "' Binding Redirect already exists");
+                continue;
+            }
+
+            dependentAssembly = new XElement(NameDependentAssembly);
+            assemblyBinding.Add(dependentAssembly);
+
+            var assemblyIdentity = new XElement(NameAssemblyIdentity);
+            dependentAssembly.Add(assemblyIdentity);
+            assemblyIdentity.SetAttributeValue(AttribName, asm.Name.Name);
+            assemblyIdentity.SetAttributeValue(AttribPublicKeyToken, pkt);
+            if (!string.IsNullOrEmpty(asm.Name.Culture))
+            {
+                assemblyIdentity.SetAttributeValue(AttribCulture, asm.Name.Culture);
+            }
+
+            var bindingRedirect = new XElement(NameBindingRedirect);
+            dependentAssembly.Add(bindingRedirect);
+            bindingRedirect.SetAttributeValue("oldVersion", "0.0.0.0-65535.65535.65535.65535");
+            bindingRedirect.SetAttributeValue("newVersion", asm.Name.Version.ToString());
         }
 
-        /// <summary> Rewrites binding Redirects in the <paramref name="configFile"/> for all Assemblies (DLLs and EXEs) in the <paramref name="searchDirectory"/> </summary>
-        public static void ReWriteBindingRedirects(this FileInfo configFile, DirectoryInfo? searchDirectory = null)
+        doc.Save(configFile.FullName);
+    }
+
+    /// <summary> Gets the first <see cref="XElement"/> with the <paramref name="xName"/> from the <paramref name="parent"/>
+    /// or creates and adds it to the <paramref name="parent"/> </summary>
+    /// <returns> the found or created Element. </returns>
+    public static XElement GetOrCreateElement(this XContainer parent, XName xName)
+    {
+        ArgumentNullException.ThrowIfNull(parent);
+        ArgumentNullException.ThrowIfNull(xName);
+
+        var configuration = parent.Element(xName);
+        if (configuration == null)
         {
-            var doc = configFile.Exists ? XDocument.Load(configFile.FullName) : new XDocument();
-
-            searchDirectory ??= configFile.Directory;
-            var configuration = doc.GetOrCreateElement("configuration");
-            var runtime = configuration.GetOrCreateElement("runtime");
-            var assemblyBinding = runtime.GetOrCreateElement(XName.Get("assemblyBinding", ns));
-
-            foreach (var file in searchDirectory!.EnumerateFiles("*.*", SearchOption.AllDirectories))
-            {
-                var ext = file.Extension;
-                if (!".dll".EqualsIgnoreCase(ext) && 
-                    !".exe".EqualsIgnoreCase(ext))
-                {
-                    Trace.TraceInformation("Ignoring: " + file);
-                    continue;
-                }
-
-                var asm = LoadAssembly(file);
-                if (asm == null) // not .NET, not valid, etc.
-                    continue;
-
-                if (asm.Name.PublicKeyToken == null || asm.Name.PublicKeyToken.Length == 0) // no strong name
-                {
-                    Console.WriteLine("Skipping '" + file + "': No public key token.");
-                    continue;
-                }
-
-                var pkt = string.Join(string.Empty, asm.Name.PublicKeyToken.Select(i => i.ToString("x2")));
-                Func<XElement, bool> cultureFunc;
-                if (string.IsNullOrEmpty(asm.Name.Culture))
-                {
-                    cultureFunc = i => i.Attribute(AttribCulture) == null || i.Attribute(AttribCulture)?.Value == "neutral";
-                }
-                else
-                {
-                    cultureFunc = i => i.Attribute(AttribCulture)?.Value == asm.Name.Culture;
-                }
-
-                var dependentAssembly = assemblyBinding
-                    .Descendants(NameDependentAssembly)
-                    .Descendants(NameAssemblyIdentity)
-                    .FirstOrDefault(i => i.Attribute(AttribName)?.Value == asm.Name.Name && i.Attribute(AttribPublicKeyToken)?.Value == pkt && cultureFunc(i))?
-                    .Parent;
-                if (dependentAssembly != null)
-                {
-                    Console.WriteLine("Ignoring '" + file + "' Binding Redirect already exists");
-                    continue;
-                }
-
-                dependentAssembly = new XElement(NameDependentAssembly);
-                assemblyBinding.Add(dependentAssembly);
-
-                var assemblyIdentity = new XElement(NameAssemblyIdentity);
-                dependentAssembly.Add(assemblyIdentity);
-                assemblyIdentity.SetAttributeValue(AttribName, asm.Name.Name);
-                assemblyIdentity.SetAttributeValue(AttribPublicKeyToken, pkt);
-                if (!string.IsNullOrEmpty(asm.Name.Culture))
-                {
-                    assemblyIdentity.SetAttributeValue(AttribCulture, asm.Name.Culture);
-                }
-
-                var bindingRedirect = new XElement(NameBindingRedirect);
-                dependentAssembly.Add(bindingRedirect);
-                bindingRedirect.SetAttributeValue("oldVersion", "0.0.0.0-65535.65535.65535.65535");
-                bindingRedirect.SetAttributeValue("newVersion", asm.Name.Version.ToString());
-            }
-
-            doc.Save(configFile.FullName);
+            configuration = new XElement(xName);
+            parent.Add(configuration);
         }
 
-        /// <summary> Gets the first <see cref="XElement"/> with the <paramref name="xName"/> from the <paramref name="parent"/>
-        /// or creates and adds it to the <paramref name="parent"/> </summary>
-        /// <returns> the found or created Element. </returns>
-        public static XElement GetOrCreateElement(this XContainer parent, XName xName)
+        return configuration;
+    }
+
+    public static AssemblyDefinition? LoadAssembly(this FileInfo path)
+    {
+        ArgumentNullException.ThrowIfNull(path);
+
+        try
         {
-            var configuration = parent.Element(xName);
-            if (configuration == null)
-            {
-                configuration = new XElement(xName);
-                parent.Add(configuration);
-            }
-
-            return configuration;
+            return AssemblyDefinition.ReadAssembly(path.FullName);
         }
-
-        public static AssemblyDefinition? LoadAssembly(this FileInfo path)
+        catch (Exception e)
         {
-            try
-            {
-                return AssemblyDefinition.ReadAssembly(path.FullName);
-            }
-            catch (Exception e)
-            {
-                Console.Error.WriteLine("Error reading '" + path + "': " + e.Message);
-                return null;
-            }
+            Console.Error.WriteLine("Ignoring '" + path + "': " + e.Message);
+            return null;
         }
-
     }
 }
--- BindingRedirectGenerator/Properties/AssemblyInfo.cs
+++ BindingRedirectGenerator/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("7a611313-a2ee-4e03-9fa7-21b17573aa38")]
 
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
