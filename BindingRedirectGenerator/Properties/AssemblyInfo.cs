a1c3dd93e21568ecc9d8fb695b3c78311d2a4ab9
Simon Mourier <simon_mourier@hotmail.com>
2026-01-03 16:55:50 +0100

    Updated to .NET 10

--- BindingRedirectGenerator/Properties/AssemblyInfo.cs
+++ BindingRedirectGenerator/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Binding Redirect Generator")]
 [assembly: AssemblyCompany("Simon Mourier")]
 [assembly: AssemblyProduct("Binding Redirect Generator")]
-[assembly: AssemblyCopyright("Copyright (C) 2019-2025 Simon Mourier. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright (C) 2019-2026 Simon Mourier. All rights reserved.")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("7a611313-a2ee-4e03-9fa7-21b17573aa38")]
